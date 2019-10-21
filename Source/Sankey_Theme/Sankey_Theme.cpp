57e35109b555a83edfd8a7d3fbce8b6a809434aa
Matthew Sankey <maxstertko@yahoo.com>
2019-10-21 10:02:14 -0400

    Theme inside folder

---
+++ Source/Sankey_Theme/Sankey_Theme.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Sankey_Theme.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Sankey_Theme, "Sankey_Theme" );
---
+++ Source/Sankey_Theme/Sankey_ThemeGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Sankey_ThemeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class SANKEY_THEME_API ASankey_ThemeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
